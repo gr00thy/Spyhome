91cadada89f896f66d027b0edaa0ffc8a875a02d
gr00thy <gr00thycom@gmail.com>
2024-05-15 07:08:36 -0500

    act titulo - nuevo menu

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-#TEXT
+from colorama import init, Fore, Back, Style
 
-print("  ██████  ██▓███ ▓██   ██▓ ██░ ██  ▒█████   ███▄ ▄███▓▓█████ ")
-print("▒██    ▒ ▓██░  ██▒▒██  ██▒▓██░ ██▒▒██▒  ██▒▓██▒▀█▀ ██▒▓█   ▀ ")
-print("░ ▓██▄   ▓██░ ██▓▒ ▒██ ██░▒██▀▀██░▒██░  ██▒▓██    ▓██░▒███   ")
-print("  ▒   ██▒▒██▄█▓▒ ▒ ░ ▐██▓░░▓█ ░██ ▒██   ██░▒██    ▒██ ▒▓█  ▄ ")
-print("▒██████▒▒▒██▒ ░  ░ ░ ██▒▓░░▓█▒░██▓░ ████▓▒░▒██▒   ░██▒░▒████▒")
-print("▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░  ██▒▒▒  ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░░ ▒░ ░")
-print("░ ░▒  ░ ░░▒ ░     ▓██ ░▒░  ▒ ░▒░ ░  ░ ▒ ▒░ ░  ░      ░ ░ ░  ░")
-print("░  ░  ░  ░░       ▒ ▒ ░░   ░  ░░ ░░ ░ ░ ▒  ░      ░      ░   ")
-print("      ░           ░ ░      ░  ░  ░    ░ ░         ░      ░  ░")
-print("                  ░ ░                                        ")
-print("v1.0")
+#TITLE
+
+init()
+print(Fore.YELLOW+"")
+print(Fore.YELLOW+"  ██████  ██▓███ ▓██   ██▓ ██░ ██  ▒█████   ███▄ ▄███▓▓█████ ")
+print(Fore.YELLOW+"▒██    ▒ ▓██░  ██▒▒██  ██▒▓██░ ██▒▒██▒  ██▒▓██▒▀█▀ ██▒▓█   ▀ ")
+print(Fore.YELLOW+"░ ▓██▄   ▓██░ ██▓▒ ▒██ ██░▒██▀▀██░▒██░  ██▒▓██    ▓██░▒███   ")
+print(Fore.YELLOW+"  ▒   ██▒▒██▄█▓▒ ▒ ░ ▐██▓░░▓█ ░██ ▒██   ██░▒██    ▒██ ▒▓█  ▄ ")
+print(Fore.YELLOW+"▒██████▒▒▒██▒ ░  ░ ░ ██▒▓░░▓█▒░██▓░ ████▓▒░▒██▒   ░██▒░▒████▒")
+print(Fore.YELLOW+"▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░  ██▒▒▒  ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░░ ▒░ ░")
+print(Fore.YELLOW+"░ ░▒  ░ ░░▒ ░     ▓██ ░▒░  ▒ ░▒░ ░  ░ ▒ ▒░ ░  ░      ░ ░ ░  ░")
+print(Fore.YELLOW+"░  ░  ░  ░░       ▒ ▒ ░░   ░  ░░ ░░ ░ ░ ▒  ░      ░      ░   ")
+print(Fore.YELLOW+"      ░           ░ ░      ░  ░  ░    ░ ░         ░      ░  ░")
+print(Fore.YELLOW+"                  ░ ░                                        ")
+print("")
+print(Fore.GREEN+"v1.0")
 print("")
 
 # MENU
 
-print("-- MENU --")
-print("1- OPCION1")
-print("2- OPCION2")
-print("3- OPCION3")
-print("4- OPCION4")
-print("5- OPCION5")
+while True:
+    menu = int(input(Fore.GREEN+"""
+    MENU
+    (1)-- Opcion 1
+    (2)-- Opcion 2 
+    (3)-- Opcion 3  
+    (4)-- Opcion 4
+    (0)-- Salir                                                
+    """))
+
+    if menu == 1:
+        print("Opcion 1")
+    elif menu == 2:
+        print("Opcion 2")
+    elif menu == 3:
+        print("Opcion 3")
+    elif menu == 4:
+        print("Opcion 4")
+    else:
+        break
